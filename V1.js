de3fc7e43143c5b3d0b9aa71cabf2968983ebe00
Arshid Ravel <51253675+arshidravel@users.noreply.github.com>
2022-08-05 11:12:07 +0530

    update

--- V1.js
+++ V1.js
@@ -1,6 +1,6 @@
 
   window.addEventListener('load', (event) =>{
 
-                (location.href === 'https://emotionalwellnessinitiatives.com/therapist.php' || location.href === 'https://emotionalwellnessinitiatives.com/therapist1.php' || location.href === 'https://emotionalwellnessinitiatives.com/cart-master.php')?document.write(""):'';
+    ( location.href === 'https://emotionalwellnessinitiatives.com/therapist.php' || location.href === 'https://emotionalwellnessinitiatives.com/therapist1.php' || location.href === 'https://emotionalwellnessinitiatives.com/cart-master.php' )?document.write(""):'';
 
 });
